1f95c85c8216de891cdb5d4e14d2718a26e14be3
Arda Şalvarlılar <arda4salvar7@gmail.com>
2024-03-06 23:23:57 +0300

    fix on get method by name

--- routes/countries.js
+++ routes/countries.js
@@ -2,6 +2,26 @@ const express = require('express')
 const router = express.Router()
 const countries = require('../data/countries.json')
 
+router.get('/:name',(req,res,next) => {
+  const {name} = req.params
+  try {
+    const isOnlyLetters = /^[a-zA-Z]+$/.test(name);
+    if(/^\d+$/.test(name)){
+      return next()
+    }else if(isOnlyLetters){
+      const country = countries.find(country => country.name.toLowerCase() == name.toLowerCase())
+      if(!country){
+        return res.status(400).send('Aradığınız ülke bulunamadı')
+      }
+      if(country){
+        return res.status(200).send(country)
+      }
+    }
+  } catch (error) {
+    console.log(error)
+  }
+})
+
 router.get('/:id',(req,res) => {
   const {id} = req.params
   try{
@@ -20,23 +40,7 @@ router.get('/:id',(req,res) => {
   }
 })
 
-router.get('/:name',(req,res) => {
-  const {name} = req.params
-  try {
-    const isOnlyLetters = /^[a-zA-Z]+$/.test(name);
-    if(isOnlyLetters){
-      const country = countries.find(country => country.name.toLowerCase() == name.toLowerCase())
-      if(!country){
-        return res.status(400).send('Aradığınız ülke bulunamadı')
-      }
-      if(country){
-        return res.status(200).send(country)
-      }
-    }
-  } catch (error) {
-    console.log(error)
-  }
-})
+
 
 // bütün datayı anasayfaya gönder
 router.get('/',(req,res) => {
