6448d708872ff4c6392aeb0e9956861a6c81f826
Arda Şalvarlılar <arda4salvar7@gmail.com>
2024-03-06 23:05:46 +0300

    all countries get method

--- routes/countries.js
+++ routes/countries.js
@@ -2,4 +2,27 @@ const express = require('express')
 const router = express.Router()
 const countries = require('../data/countries.json')
 
+router.get('/:id',(req,res) => {
+  const {id} = req.params
+  try{
+    const isNumeric = /^\d+$/.test(id);
+    if(isNumeric){
+      const country = countries.find(country => country.id == Number(id))
+      if(!country){
+        return res.status(400).send('Aradığınız ülke bulunamadı')
+      }
+      if(country){
+        return res.status(200).send(country)
+      }
+    }
+  }catch(error){
+    console.log(error)
+  }
+})
+
+// bütün datayı anasayfaya gönder
+router.get('/',(req,res) => {
+  res.status(200).send(countries)
+})
+
 module.exports = router
