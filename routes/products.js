9397c2a1540537139ab748fdd8ca4d5660b995b5
Arda Şalvarlılar <arda4salvar7@gmail.com>
2024-03-08 22:12:37 +0300

    product data and routes

--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,26 @@ const express = require('express')
 const router = express.Router()
 const products = require('../data/products.json')
 
+router.get('/:name',(req,res,next) => {
+  const {name} = req.params
+  const isNumeric = /^\d+$/.test(name);
+  const isOnlyLetters = /^[a-zA-Z]+$/.test(name);
+
+  try {
+    if(isOnlyLetters && !isNumeric){
+      const product = products.filter((product) => product.name.toLowerCase() == name.toLowerCase())
+      if(!product){
+        return res.status(400).send('Aradığınız ürün bulunamadı')
+      }
+      return res.status(200).send(product)
+    }else if(isNumeric && !isOnlyLetters){
+      next()
+    }
+  } catch (error) {
+    
+  }
+})
+
 router.get('/:id',(req,res,next) => {
   const {id} = req.params
   const product = products.find(product => product.id == id)
